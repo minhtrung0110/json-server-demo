fd2e2289f0dd63bc8d1041be232fa6ce7d2f3bac
Minh Trung <nguyenducminhtrung0044@gmail.com>
2022-07-15 21:33:52 +0700

    Generate date

---
+++ README.md
@@ -0,0 +1,17 @@
+## Giới Thiệu
+
+
+
+## Các chức năng
+Truy cập website để đọc hướng dẫn fake các loại data : https://www.npmjs.com/package/faker/v/5.3.0
+
+
+## Cấu hình
+
+
+## Các Libs để fake data :
+Hiện tại Faker đã die<br>
+Project đang sử dụng Faker phiên bản 5.0<br>
+Bên cạnh đó cũng đẵ install 2 libs là causual và @faker-js/faker
+
+## Hướng Dẫn Sử Dụng
---
+++ generate-data.js
@@ -0,0 +1,142 @@
+const faker = require('faker');
+const fs = require('fs');
+
+
+// Set locale to use Vietnamese
+faker.locale = "vi";
+
+// random data
+const randomCategoryList = (n) => {
+  if (n <= 0) return [];
+
+  const categoryList = [];
+
+  // loop and push category
+  Array.from(new Array(n)).forEach(() => {
+    const category = {
+      id: faker.random.uuid(),
+      name: faker.commerce.department(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    categoryList.push(category);
+  });
+
+  return categoryList;
+};
+const randomProductList = (categoryList,n) => {
+    if (n <= 0) return [];
+  
+    const productList = [];
+  
+    for( const category of categoryList){
+        Array.from(new Array(n)).forEach(() => {
+            const product = {
+              id: faker.random.uuid(),
+              name: faker.commerce.productName(),
+              categoryId:category.id,
+              price:faker.commerce.price(),
+              color:faker.commerce.color(),
+              description:faker.commerce.productDescription(),
+              thumbnailUrl:faker.image.imageUrl(450,420),
+              createdAt: Date.now(),
+              updatedAt: Date.now(),
+            };
+        
+            productList.push(product);
+          });
+    }  
+    return productList;
+  };
+const randomCustomerList = (n) => {
+    if (n <= 0) return [];
+  
+    const customerList = [];
+  
+    // loop and push category
+    Array.from(new Array(n)).forEach(() => {
+      const customer = {
+        id: faker.random.uuid(),
+        first_name: faker.name.firstName(),
+        last_name:faker.name.lastName(),
+        email:faker.internet.email(),
+        phone:faker.phone.phoneNumber(),
+        addresss:faker.address.streetAddress(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      };
+  
+      customerList.push(customer);
+    });
+  
+    return customerList;
+  };
+const randomInvoiceList = (customerList,n) => {
+    if (n <= 0) return [];
+  
+    const invoiceList = [];
+  
+    for(const customer of customerList) {
+        Array.from(new Array(n)).forEach(() => {
+            const invoice = {
+              id: faker.random.uuid(),
+              customerId:customer.id,
+              total:faker.commerce.price(),
+              createdAt: Date.now(),
+              updatedAt: Date.now(),
+            };
+        
+            invoiceList.push(invoice);
+          });
+        
+    }
+   
+    return invoiceList;
+  };
+const randomDetailInvoiceList = (invoiceList,productList,n) => {
+    if (n <= 0) return [];
+  
+    const detail_invoiceList = [];
+    for(const invoice of invoiceList){
+        for(const product of productList) {
+            Array.from(new Array(n)).forEach(() => {
+                const  detail_invoice = {
+                  id: faker.random.uuid(),
+                  invoiceId:invoice.id,
+                  productId:product.id,
+                  amount:faker.finance.amount(),
+                  price:product.price,
+                  createdAt: Date.now(),
+                  updatedAt: Date.now(),
+                };
+            
+                detail_invoiceList.push( detail_invoice);
+              });
+            
+        }
+    }
+   
+   
+    return  detail_invoiceList;
+  };
+// prepare db object
+const categories=randomCategoryList(2)
+const products=randomProductList(categories,3)
+const customers=randomCustomerList(1)
+const invoices=randomInvoiceList(customers,1)
+const detail_invoices=randomDetailInvoiceList(invoices,products,2)
+const db={
+    "categories": categories,
+    "products": products,
+    "customers":customers,
+    "invoices": invoices,
+    "detail_invoices": detail_invoices
+  }
+
+
+
+// write db object to db.json
+  fs.writeFile("db.json",JSON.stringify(db),()=>{
+    console.log("Generate data successfully")
+  })
